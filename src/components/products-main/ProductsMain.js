5bd6fbaf110512e8297729d072b192b88f9ebe13
Andrés López <andreslp08@hotmail.com>
2021-04-23 20:56:10 -0500

    Now products navbar show every section with products

--- src/components/products-main/ProductsMain.js
+++ src/components/products-main/ProductsMain.js
@@ -3,70 +3,73 @@ import { ProductsBar } from '../products-bar/ProductsBar';
 import {
     BrowserRouter as Router,
     Switch,
-    Route
+    Route,
+    NavLink,
+    Link,
+    Redirect,
+    withRouter,
+    useParams
+
 } from "react-router-dom";
 import { PAGE_ROUTES } from '../../models/PageRoutes';
 import { ProductCard } from '../product-card/ProductCard';
 import { ProductsController } from '../../controllers/ProductsController';
+import { PRODUCTS_DB } from '../../models/database-simulation/Products';
+import { ProductsSection } from '../products-section/ProductsSection';
+import { CATEGORIES } from '../../models/Categories';
 
-export class ProductsMain extends React.Component {
 
+ class ProductsMain extends React.Component {
 
     constructor(props) {
         super(props)
-        this.state={
-            products:[<ProductCard/>,<ProductCard/>]
-        }
-    }
-
-    componentDidMount(){
-        this.setState({products:[<ProductCard/>,<ProductCard/>,<ProductCard/>]})
-    }
-    componentDidUpdate(){
-    }
-    componentWillUnmount(){
-        
-        console.log("hola")
     }
 
     render() {
+        const section = this.props.match.params.section;
         return (
-            <Router>
-                <section className="flex below-navbar relative flex-col sm:flex-row">
+            <section className="flex below-navbar relative flex-col sm:flex-row">
                     <ProductsBar />
                     <section className="flex flex-grow flex-wrap flex-row items-center justify-center">
-                        <Route path={PAGE_ROUTES.products.guitars} >
-                            {this.renderProducts()}
+                        {
+
+                            section.toLowerCase() === "all"?
+                            <ProductsSection sectionName={section} productsData={ProductsController.getAllProducts()} />:
+                            <ProductsSection sectionName={section} productsData={ProductsController.getAllProductsBySection(section)} />
+                        }
+                      
+                        {/* <Route exact path={PAGE_ROUTES.products.all} >
+                            <ProductsSection sectionName={"All"} productsData={ProductsController.getAllProducts()} />
                         </Route>
-                        <Route path={PAGE_ROUTES.products.basses}>
-                            <div className="color-onbackground">Basses</div>
+                        <Route exact path={PAGE_ROUTES.products.guitars} >
+                            <ProductsSection sectionName={"Guitars"} productsData={ProductsController.getAllProductsBySection('guitars')} />
                         </Route>
-                        <Route path={PAGE_ROUTES.products.violins}>
-                            <div className="color-onbackground">Violins</div>
+                        <Route exact path={PAGE_ROUTES.products.basses} >
+                            <ProductsSection sectionName={"Basses"} productsData={ProductsController.getAllProductsBySection('basses')} />
                         </Route>
-                        <Route path={PAGE_ROUTES.products.clarinets}>
+                        <Route exact path={PAGE_ROUTES.products.violins}>
+                            <ProductsSection sectionName={"Violins"} productsData={ProductsController.getAllProductsBySection('violins')} />
+                        </Route>
+                        <Route exact path={PAGE_ROUTES.products.clarinets}>
                             <div className="color-onbackground">Clarinets</div>
                         </Route>
-                        <Route path={PAGE_ROUTES.products.flutes}>
+                        <Route exact path={PAGE_ROUTES.products.flutes}>
                             <div className="color-onbackground">Flutes</div>
                         </Route>
-                        <Route path={PAGE_ROUTES.products.saxophones}>
+                        <Route exact path={PAGE_ROUTES.products.saxophones}>
                             <div className="color-onbackground">Saxophones</div>
                         </Route>
-                        <Route path={PAGE_ROUTES.products.trumpets}>
+                        <Route exact path={PAGE_ROUTES.products.trumpets}>
                             <div className="color-onbackground">Trumpets</div>
-                        </Route>
+                        </Route> */}
                     </section>
+            </section>
 
-                </section>
-            </Router>
+
 
         )
     }
 
-    renderProducts(){
-       return this.state.products.map(()=>{
-            return <div>hola</div>
-        })
-    }
 }
+
+export default withRouter(ProductsMain);
