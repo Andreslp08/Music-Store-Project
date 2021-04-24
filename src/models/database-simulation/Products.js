f5b7af9d6afb58799658ca851e5c9d10b35bea96
Andrés López <andreslp08@hotmail.com>
2021-04-23 20:52:54 -0500

    database simulation updated

--- src/models/database-simulation/Products.js
+++ src/models/database-simulation/Products.js
@@ -1,32 +1,99 @@
 export const PRODUCTS_DB = [
     { 
-        id: 1, 
-        name: "Product 1", 
+        id: 1,
+        section:"guitars", 
+        name: "A", 
         imgPath: "", 
-        price: "50000", 
+        price: 300, 
         details: "Details product 1", 
-        isNew: true, 
-        isOff: false, 
-        off: "" 
+        isNew: false, 
+        isPromo: false, 
+        promo: "",
+        isSoldOut:false 
     },
     { 
         id: 2, 
-        name: "Product 2", 
+        section:"guitars", 
+        name: "B", 
         imgPath: "", 
-        price: "60000", 
+        price: 200, 
         details: "Details product 2", 
         isNew: true, 
-        isOff: true, 
-        off: "50% 0ff" 
+        isPromo: true, 
+        promo: "50% 0ff",
+        isSoldOut:true 
     },
     { 
         id: 3, 
-        name: "Product 3", 
+        section:"guitars", 
+        name: "C", 
         imgPath: "", 
-        price: "60000", 
+        price: 10000, 
         details: "Details product 3", 
         isNew: false, 
-        isOff: true, 
-        off: "50% 0ff" 
+        isPromo: true, 
+        promo: "50% 0ff",
+        isSoldOut:true  
+    },
+    { 
+        id: 4,
+        section:"basses", 
+        name: "D", 
+        imgPath: "", 
+        price: 4000, 
+        details: "Details product 1", 
+        isNew: true, 
+        isPromo: false, 
+        promo: "" ,
+        isSoldOut:true 
+    },
+    { 
+        id: 5, 
+        section:"basses", 
+            name: "E", 
+        imgPath: "", 
+        price: 1000000, 
+        details: "Details product 2", 
+        isNew: true, 
+        isPromo: true, 
+        promo: "50% 0ff",
+        isSoldOut:true  
+    },
+    { 
+        id: 6, 
+        section:"violins", 
+        name: "F", 
+        imgPath: "", 
+        price: 99999, 
+        details: "Details product 3", 
+        isNew: false, 
+        isPromo: true, 
+        promo: "50% 0ff",
+        isSoldOut:true  
+    },
+    { 
+        id: 7, 
+        section:"violins", 
+        name: "G", 
+        imgPath: "", 
+        price: 5555, 
+        details: "Details product 3", 
+        isNew: false, 
+        isPromo: true, 
+        promo: "50% 0ff" ,
+        isSoldOut:true 
+    },
+    ,
+    { 
+        id: 8, 
+        section:"violins", 
+        name: "H", 
+        imgPath: "", 
+        price: 60000, 
+        details: "Details product 3", 
+        isNew: false, 
+        isPromo: true, 
+        promo: "50% 0ff" ,
+        isSoldOut:true 
     }
 ]
