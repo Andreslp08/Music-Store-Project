8f08fc94aeaa6962e55e0a3ff2f2cd676a3adec4
Andrés López <andreslp08@hotmail.com>
2021-05-02 15:14:41 -0500

    tag error fixed

--- src/components/notfound/NotFound.js
+++ src/components/notfound/NotFound.js
@@ -13,10 +13,10 @@ class NotFound extends React.Component {
                 <h1 className="color-onbackground text-3xl font-roboto m-2 text-center">OOPS!</h1>
                 <h3 className="font-anton color-primary border-solid border-4  p-2 text-9xl animate-pulse">404</h3>
                 <h1 className="color-onbackground text-3xl font-roboto m-2 text-center">Page not found</h1>
-                <secton className="flex flex-wrap items-center justify-center">
+                <section className="flex flex-wrap items-center justify-center">
                     <Link className="primary-button" to={PAGE_ROUTES.home}>Return to safe page!</Link>
                     <Link className="variant-button rounded-full" to={PAGE_ROUTES.home}>Report</Link>
-                </secton>
+                </section>
              
             </section>
         )
