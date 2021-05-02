4192d0197132d67c8971edc671cdeebbf33eaed5
Andrés López <andreslp08@hotmail.com>
2021-05-02 15:15:37 -0500

    sign in button added

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { Swicth } from '../switch/Switch'
 import './Navbar.css'
-import { NavLink } from "react-router-dom";
+import { NavLink, withRouter } from "react-router-dom";
 import { PAGE_ROUTES } from '../../models/PageRoutes';
 
-export class Navbar extends React.Component {
+ class Navbar extends React.Component {
 
     constructor(props) {
         super(props)
@@ -22,7 +22,11 @@ export class Navbar extends React.Component {
                     <div ref={this.hamburgerMenu} className="hamburger-menu" data-state="hide">
                         <label className="hamburger-line"></label>
                     </div>
-                    <h1 className="m-2 font-anton text-xl color-onbackground cursor-pointer" ><NavLink to={PAGE_ROUTES.home}  onClick={()=>{ this.showMenu(false)}}>Music store</NavLink></h1>
+                    <h1 className="m-2 font-anton text-md sm:text-xl color-onbackground cursor-pointer text-center" ><NavLink to={PAGE_ROUTES.home}  onClick={()=>{ this.showMenu(false)}}>Music store</NavLink></h1>
+                    <div className="flex justify-center items-center m-1">
+                                <i className="fas fa-moon fa-1x p-1 color-onbackground "></i>
+                                <Swicth ref={this.switchRef} />
+                            </div>
                     <div className="navbar-main flex items-center sm:justify-between">
                         <ul className="list flex justify-center items-center">
                             <li className="item"><NavLink to={PAGE_ROUTES.home} activeStyle={{ color: 'var(--primaryColor' }} onClick={()=>{ this.showMenu(false)}}>Home</NavLink></li>
@@ -30,16 +34,16 @@ export class Navbar extends React.Component {
                             <li className="item"><NavLink to="/t" activeStyle={{ color: 'var(--primaryColor' }}  onClick={()=>{ this.showMenu(false)}}>Contact</NavLink></li>
                         </ul>
                         <div className="navbar-user-section flex justify-center items-center flex-row mx-4 flex-wrap">
-                            <div className="flex justify-center items-center m-1">
-                                <i className="fas fa-moon fa-1x p-1 color-onbackground "></i>
-                                <Swicth ref={this.switchRef} />
-                            </div>
                             <div className="flex flex-nowrap justify-center items-center">
                                 <button className="rounded-full  m-2 color-onbackground hover-scale"  onClick={()=>{ this.showMenu(false)}}>
-                                    <i className="fas fa-shopping-cart"></i>
+                                    <i className="fas fa-shopping-cart hover:text-indigo-700"></i>
                                 </button>
-                                <p className="p-1 color-onbackground text-center">Bryan Andrés López Palacios</p>
-                                <img src="https://dt2sdf0db8zob.cloudfront.net/wp-content/uploads/2019/12/9-Best-Online-Avatars-and-How-to-Make-Your-Own-for-Free-image1-5.png" className="rounded-full w-9 h-9 hover-scale m-1 mx-2 border-none"  onClick={()=>{ this.showMenu(false)}}></img>
+                                <button className="rounded-full  m-2 color-onbackground hover-scale"  onClick={()=>{ this.showMenu(false)}}>
+                                    <i className="fas fa-heart hover:text-red-500"></i>
+                                </button>
+                                <button className="primary-button rounded-full py-1">Sign In</button>
+                                {/* <p className="p-1 color-onbackground text-center">Bryan Andrés López Palacios</p>
+                                <img src="https://dt2sdf0db8zob.cloudfront.net/wp-content/uploads/2019/12/9-Best-Online-Avatars-and-How-to-Make-Your-Own-for-Free-image1-5.png" className="rounded-full w-9 h-9 hover-scale m-1 mx-2 border-none"  onClick={()=>{ this.showMenu(false)}}></img> */}
                             </div>
                         </div>
                     </div>
@@ -120,3 +124,5 @@ export class Navbar extends React.Component {
 
 
 }
+
+export default withRouter(Navbar);
