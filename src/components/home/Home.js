680601c13a7395652fc0abb92ab6e8d3c5213e27
Andrés López <andreslp08@hotmail.com>
2021-05-22 13:25:36 -0500

    theme color changed

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -14,7 +14,7 @@ export class Home extends React.Component{
     Welcome(){
         return (
             <div className="welcome-img flex flex-col justify-center items-center">
-                <h2 className="text-center bg-background color-onbackground font-anton p-8 rounded-full lg:text-8xl text-7xl text-white text-shadow-3"><i className="fas fa-music"></i> Music Store <i className="fas fa-music"></i></h2>
+                <h2 className="text-center bg-background color-onbackground font-anton p-8 rounded-full lg:text-8xl text-7xl text-shadow-3"><i className="fas fa-music"></i> Music Store <i className="fas fa-music"></i></h2>
                 <h3 className="m-5 text-white text-xl text-shadow-2">We have the best instruments with good prices!</h3>
             </div>
         )
