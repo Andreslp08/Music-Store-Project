674321ec2d68760712fa703be634f84ae48ecec1
Andrés López <andreslp08@hotmail.com>
2021-06-10 13:39:07 -0500

    redux added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import  Navbar  from './components/navbar/Navbar';
+import Navbar from './components/navbar/Navbar';
 import './App.css';
 import {
   BrowserRouter as Router,
@@ -17,57 +17,54 @@ import SignIn from './components/sign-in/SignIn';
 import SignUp from './components/sign-up/SignUp';
 import Favorites from './components/favorites/Favorites';
 import Cart from './components/cart/Cart';
+import { Provider } from 'react-redux';
+import store from './redux/store/Store'
 
 function App() {
-  // windowSizes();
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-      </div>
-      <main>
-        <Switch>
-          <Route exact path={PAGE_ROUTES.home}>
-            <Home />
-          </Route>
-          <Route
-            exact path={PAGE_ROUTES.products.sectionParams}
-            render={(props) => (
-              <ProductsMain key={window.location.pathname} />)} />
-          <Route
-            exact path={PAGE_ROUTES.products.productParams}
-            render={(props) => (
-              <ProductView key={window.location.pathname} />)} />
-          <Route exact path="/">
-            <Redirect to={PAGE_ROUTES.home}/>
-          </Route>
-          <Route path={PAGE_ROUTES.signIn}  >
-            <SignIn/>
-          </Route>
-          <Route path={PAGE_ROUTES.signUp}  >
-            <SignUp/>
-          </Route>
-          <Route path={PAGE_ROUTES.favorites} >
-            <Favorites/>
-          </Route>
-          <Route path={PAGE_ROUTES.cart} >
-            <Cart/>
-          </Route>
-          <Route path='*' exact={true} >
-            <NotFound/>
-          </Route>
-        </Switch>
-      </main>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Navbar />
+        </div>
+        <main>
+          <Switch>
+            <Route exact path={PAGE_ROUTES.home}>
+              <Home />
+            </Route>
+            <Route
+              exact path={PAGE_ROUTES.products.sectionParams}
+              render={(props) => (
+                <ProductsMain key={window.location.pathname} />)} />
+            <Route
+              exact path={PAGE_ROUTES.products.productParams}
+              render={(props) => (
+                <ProductView key={window.location.pathname} />)} />
+            <Route exact path="/">
+              <Redirect to={PAGE_ROUTES.home} />
+            </Route>
+            <Route path={PAGE_ROUTES.signIn}  >
+              <SignIn />
+            </Route>
+            <Route path={PAGE_ROUTES.signUp}  >
+              <SignUp />
+            </Route>
+            <Route path={PAGE_ROUTES.favorites} >
+              <Favorites />
+            </Route>
+            <Route path={PAGE_ROUTES.cart} >
+              <Cart />
+            </Route>
+            <Route path='*' exact={true} >
+              <NotFound />
+            </Route>
+          </Switch>
+        </main>
+      </Router>
+    </Provider>
+
   );
 }
 
-// function windowSizes() {
-//   window.onresize = () => {
-//     if (!window.matchMedia('(max-width:800px)').matches || !window.matchMedia('(max-width:600px)').matches) {
-//       document.body.style.overflow = "auto";
-//     }
-//   };
-// }
 
 export default App;
