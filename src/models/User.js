2a2fe06f6a9d5ff30d64d6b8031604c63c4d8a73
Andrés López <andreslp08@hotmail.com>
2021-09-07 16:43:38 -0500

    avatarPath added

--- src/models/User.js
+++ src/models/User.js
@@ -1,10 +1,13 @@
 export class User {
-    constructor(id,email, firstName, lastName, password){
+
+    static defaultAvatarPath = "https://openclipart.org/download/247319/abstract-user-flat-3.svg"
+    constructor(id,email, firstName, lastName, password, avatarPath = User.defaultAvatarPath){
         this.id = id;
         this.email = email;
         this.firstName = firstName;
         this.lastName = lastName;
         this.password = password;
+        this.avatarPath = avatarPath;
     }
     
 }
