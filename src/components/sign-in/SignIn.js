4da036572a034b7aaf51e13e912aa357f7a772e4
Andrés López <andreslp08@hotmail.com>
2021-09-07 16:46:49 -0500

    remember credentials, load credentials and auth with google implemented

--- src/components/sign-in/SignIn.js
+++ src/components/sign-in/SignIn.js
@@ -1,7 +1,7 @@
 import { signInWithCredential } from '@firebase/auth';
 import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
-import { defaultSignIn } from '../../controllers/authController';
+import { defaultSignIn, googleSignIn } from '../../controllers/authController';
 import { PAGE_ROUTES } from '../../models/PageRoutes';
 import { Swicth } from '../switch/Switch';
 
@@ -10,6 +10,14 @@ class SignIn extends React.Component {
         super(props);
         this.emailRef = React.createRef();
         this.passwordRef = React.createRef();
+        this.rememberSwitchRef = React.createRef();
+        this.state = {
+            remember: false
+        }
+    }
+
+    componentDidMount() {
+        this.loadCredentials();
     }
 
     render() {
@@ -23,32 +31,61 @@ class SignIn extends React.Component {
                         <Link className="variant-button rounded-md" to={PAGE_ROUTES.signUp}>Sign Up</Link>
                     </section>
                     <section className="flex flex-col items-center justify-center">
-                        <form className="flex flex-col items-center justify-center" onSubmit={(e)=>{this.sigIn();e.preventDefault()}}>
+                        <form className="flex flex-col items-center justify-center" onSubmit={(e) => { this.sigIn(); e.preventDefault() }}>
                             <input className="textfield primary-textfield" type="email" placeholder="Email" ref={this.emailRef} required></input>
-                            <input className="textfield primary-textfield" type="password" placeholder="Password" ref = {this.passwordRef} required></input>
+                            <input className="textfield primary-textfield" type="password" placeholder="Password" ref={this.passwordRef} required></input>
                             <div className="flex flex-wrap items-center">
                                 <h3 className="color-onbackground m-2 font-roboto">Remember me</h3>
-                                <Swicth />
+                                <Swicth ref={this.rememberSwitchRef} />
                             </div>
                             <Link className="m-2 text-center text-xs color-onbackground font-roboto" to="/recover-password">Forgot password ? Click here</Link>
                             <input className="primary-button rounded-full cursor-pointer" type="submit" value="Sign In" required></input>
                         </form>
                         <h3 className="color-onbackground m-2 font-anton font-roboto font-bold">Or Sign in with</h3>
-                        <button className="secondary-button rounded-full p-2">Google</button>
+                        <button className="secondary-button rounded-full p-2" onClick={() => { this.signInWithGoogle() }}>Google</button>
                     </section>
                 </div>
             </div>
         )
     }
-    sigIn(){
-        defaultSignIn(
-            this.emailRef.current.value,
-            this.passwordRef.current.value
-        ).then((userCredential)=>{
+
+    sigIn() {
+        const email = this.emailRef.current.value;
+        const password = this.passwordRef.current.value;
+        defaultSignIn(email, password).then((userCredential) => {
             alert("Welcome " + userCredential.user.email);
-        }).catch(error=>{
+            if (this.rememberSwitchRef.current.isActivated()) {
+                this.saveCredentials(email, password)
+            } else {
+                localStorage.removeItem('user-credentials');
+            }
+            this.props.history.push(PAGE_ROUTES.home)
+        }).catch(error => {
             alert(error);
         })
+    }
+
+    signInWithGoogle() {
+        googleSignIn().then(result => {
+            this.props.history.push(PAGE_ROUTES.home)
+        });
+    }
+
+    saveCredentials(email, password) {
+        localStorage.setItem("user-credentials", JSON.stringify({
+            email: email,
+            password: password
+        }))
+        console.log("Credentials was saved");
+    }
+
+    loadCredentials() {
+        const credentials = JSON.parse(localStorage.getItem('user-credentials'));
+        if (credentials) {
+            this.emailRef.current.value = credentials.email;
+            this.passwordRef.current.value = credentials.password;
+            this.rememberSwitchRef.current.setActive(true);
+        }
     }
 }
 
