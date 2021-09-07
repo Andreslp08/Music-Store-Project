ad25f97b0f72169f12216ef9af43543d6013d0ff
Andrés López <andreslp08@hotmail.com>
2021-09-07 01:37:39 -0500

    now accounts can be created and saved

--- src/components/sign-up/SignUp.js
+++ src/components/sign-up/SignUp.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import {withRouter, Link} from 'react-router-dom';
 import { PAGE_ROUTES } from '../../models/PageRoutes';
-import { Swicth } from '../switch/Switch';
+import {createAuthWith, PROVIDERS} from '../../controllers/authController'
+import { User } from '../../models/User';
 
 
 class SignUp extends React.Component{
     constructor(props){
         super(props);
+        this.emailRef = React.createRef();
+        this.firstNamesRef = React.createRef();
+        this.lastNamesRef = React.createRef();
+        this.firstNamesRef = React.createRef();
+        this.passwordRef = React.createRef();
+        this.confirmPasswordRef = React.createRef();
     }
 
     render() {
@@ -19,22 +26,41 @@ class SignUp extends React.Component{
                         <h3 className="m-2 text-center text-md color-onprimary font-roboto">Do you have an account?</h3>
                         <Link className="variant-button rounded-md" to={PAGE_ROUTES.signIn}>Sign In</Link>
                     </section>
-                    <section className="flex flex-col items-center justify-center">
-                        <form className="flex flex-col items-center justify-center">
+                    <section className="flex flex-col items-center justify-center" >
+                        <form className="flex flex-col items-center justify-center" onSubmit={(e)=>{ this.createAccount(); e.preventDefault()}} >
                             <div className="flex flex-wrap items-center justify-center">
-                            <input className="textfield primary-textfield" type="email" placeholder="Email" required></input>
-                            <input className="textfield primary-textfield" type="email" placeholder="First names" required></input>
-                            <input className="textfield primary-textfield" type="email" placeholder="Last names" required></input>
-                            <input className="textfield primary-textfield" type="password" placeholder="Password" required></input>
-                            <input className="textfield primary-textfield" type="password" placeholder="Confirm password" required></input>
+                            <input className="textfield primary-textfield" type="email" placeholder="Email" ref={this.emailRef} required></input>
+                            <input className="textfield primary-textfield" type="text" placeholder="First names" ref={this.firstNamesRef} required></input>
+                            <input className="textfield primary-textfield" type="text" placeholder="Last names" ref={this.lastNamesRef} required></input>
+                            <input className="textfield primary-textfield" type="password" placeholder="Password" ref={this.passwordRef} minLength="8" required></input>
+                            <input className="textfield primary-textfield" type="password" placeholder="Confirm password" ref={this.confirmPasswordRef} minLength="8" required></input>
                             </div>
-                            <input className="primary-button rounded-full cursor-pointer" type="submit" value="Sign Up" required></input>
+                            <input className="primary-button rounded-full cursor-pointer" type="submit" value="Sign Up"  required></input>
                         </form>
                     </section>
                 </div>
             </div>
         )
     }
+
+
+    createAccount(){
+        if( this.passwordRef.current.value != this.confirmPasswordRef.current.value){
+            alert("Passwords are not the same");
+            return;
+        }
+        const user = new User(
+            0,
+            this.emailRef.current.value,
+            this.firstNamesRef.current.value,
+            this.lastNamesRef.current.value,
+            this.passwordRef.current.value
+        )
+        createAuthWith(PROVIDERS.default,user).then(res=>{
+            this.props.history.push(PAGE_ROUTES.signIn)
+        });
+    }
+
 }
 
 export default withRouter(SignUp);
