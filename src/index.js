ad2fca4bd2570509d2f58b446ff2ce6700328d54
Andrés López <andreslp08@hotmail.com>
2021-09-07 01:33:22 -0500

    firebase added

--- src/index.js
+++ src/index.js
@@ -4,6 +4,30 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import * as Auth from 'firebase/auth'
+import * as Firestore from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCTYnoSdu1MbthmAuOx75JfwQ4Ocbe-brU",
+  authDomain: "music-store-df00c.firebaseapp.com",
+  projectId: "music-store-df00c",
+  storageBucket: "music-store-df00c.appspot.com",
+  messagingSenderId: "136992465196",
+  appId: "1:136992465196:web:cb03bb7e9eccbd4d8b8c89",
+  measurementId: "G-CV7JZ72VRB"
+};
+
+// Initialize Firebase
+
+
+    const firebaseApp = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(firebaseApp);
+    Auth.connectAuthEmulator(Auth.getAuth(), "http://localhost:9099");
+    Firestore.connectFirestoreEmulator(Firestore.getFirestore(),"localhost", 8989);
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
